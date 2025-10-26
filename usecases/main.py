43ad7c9b2f6750825e7a22750e36b92c65dc756f
GaneshPublic <banda.ganesh.bly@gmail.com>
2025-10-26 18:52:55 +0100

    Python examples

---
+++ usecases/main.py
@@ -0,0 +1,10 @@
+# Sample file
+from sample import hello
+
+def main_function():
+    print("Hello, this is the main function.")
+    hello()
+
+    
+if __name__ == "__main__":
+    main_function()
---
+++ usecases/sample.py
@@ -0,0 +1,4 @@
+# This is a sample file for the use case
+
+def hello():
+    print("Hello there from sample file")
